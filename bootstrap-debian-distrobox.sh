ef469a83716950c03636426e1510c1ddf6f69d9b
namalsk <namalsk@perpsuite.com>
2024-11-03 14:56:12 +0000

    Unsure if installing non standard?

--- bootstrap-debian-distrobox.sh
+++ bootstrap-debian-distrobox.sh
@@ -7,7 +7,7 @@ if [ ! -f /etc/sudoers.d/zz_${USER} ]; then
 	echo -e "$USER ALL=(ALL) NOPASSWD: ALL\nDefaults lecture = never" | sudo tee /etc/sudoers.d/zz_${USER} > /dev/null
 fi
 
-"/home/administrator/.dotfiles/.local/bin/dotfiles.sh" vps
-"/home/administrator/.dotfiles/.local/bin/debian-install.sh" vps
+/home/administrator/.dotfiles/.local/bin/dotfiles.sh vps
+/home/administrator/.dotfiles/.local/bin/debian-install.sh vps
 ln -s /home/administrator/hacking/ ${HOME}/hacking
 sudo apt install -y --reinstall "/home/administrator/builds/debian/debs/hacking-tools/hacking-tools.deb"
