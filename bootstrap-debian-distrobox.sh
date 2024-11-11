0eced23e4ff54a4c5d11067f6374b40b0af4343d
namalsk <namalsk@perpsuite.com>
2024-11-11 12:57:27 +0000

    Changes

--- bootstrap-debian-distrobox.sh
+++ bootstrap-debian-distrobox.sh
@@ -2,16 +2,17 @@
 
 #Debian Distrobox Bootstrap
 
-hacking_tools="/home/administrator/builds/debian/debs/hacking-tools/hacking-tools.deb"
+hacking_tools_deb="/home/administrator/builds/debian/debs/hacking-tools/hacking-tools.deb"
 hacking_dir="/home/administrator/hacking"
+dotfiles_script="/home/administrator/.dotfiles/.local/bin/dotfiles.sh"
 
 # Add sudo user
 if [ ! -f /etc/sudoers.d/zz_${USER} ]; then
 	echo -e "$USER ALL=(ALL) NOPASSWD: ALL\nDefaults lecture = never" | sudo tee /etc/sudoers.d/zz_${USER} > /dev/null
 fi
 
-/home/administrator/.dotfiles/.local/bin/dotfiles.sh vps
+$dotfiles_script vps
 source ~/.bashrc
 debian-install.sh vps
 ln -s "$hacking_dir" $HOME/hacking
-sudo apt install -y --reinstall "$hacking_tools"
+sudo apt install -y --reinstall "$hacking_tools_deb"
